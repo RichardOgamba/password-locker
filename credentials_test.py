4d90bf96785c394e772be37e6f6a9bd65be944d8
Richard Ogamba <ogambarichard20@gmail.com>
2018-02-16 07:40:34 +0300

    add content to the file

--- credentials_test.py
+++ credentials_test.py
@@ -11,12 +11,12 @@ class TestCredentials(unittest.TestCase):
     Args:
         unittest.TestCase: Testcase class that helps in creating test cases
     '''
-    def setUp(self):
-        '''
-        set up method to before each test case.
-        '''
-
-        self.new_credentials = Credentials("IG", "_thrills", "jkodhe@gmail.com", "password")
+    # def setUp(self):
+    #     '''
+    #     set up method to before each test case.
+    #     '''
+    #
+    #     self.new_credentials = Credentials("IG", "_thrills", "jkodhe@gmail.com", "password")
 
     def tearDown(self):
         '''
